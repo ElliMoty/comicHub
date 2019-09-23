982962f155992a97126aa9549dfce41cb88785b5
Elli <ellimoty@gmail.com>
2019-09-23 11:02:02 +1000

    work on comic list style

--- src/app/comic.service.ts
+++ src/app/comic.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Comics } from "./comics";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
 
 @Injectable()
 export class ComicHubService {
